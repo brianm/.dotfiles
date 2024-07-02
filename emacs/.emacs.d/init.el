c0b2883857e4225626f06edec83f73aeafc84d08
Brian McCallister <brianm@skife.org>
2024-07-02 09:19:10 -0700

    update apile of emacs packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -39,5 +39,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(package-selected-packages '(counsel ivy markdown-mode nord-theme))
+ '(package-selected-packages
+   '(lsp-treemacs flycheck lsp-mode counsel ivy markdown-mode nord-theme))
  '(tool-bar-mode nil))
