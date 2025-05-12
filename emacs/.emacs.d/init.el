6b8178d7a18182483c7e2c1bfcb7c7a71fe6758e
Brian McCallister <bmcc@meta.com>
2025-05-12 14:29:06 -0700

    remove bad kanagawa

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,7 +7,7 @@
 			  "~/bin")))
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/elisp/")
