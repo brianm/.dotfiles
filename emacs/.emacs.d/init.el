325b5439b4634a83dbd64b214d43a81fc8608dc2
Brian McCallister <bmcc@meta.com>
2025-05-12 14:39:04 -0700

    kanagawa theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,8 +24,11 @@
 (setq visible-bell nil) ;; The default
 (setq ring-bell-function 'ignore)
 (setq default-tab-width 4)
-;(load-theme 'nord t)
-(load-theme 'tokyo t)
+;;;(load-theme 'nord t)
+;;;(load-theme 'tokyo t)
+(load-theme 'kanagawa-wave)
+
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -39,7 +42,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c20728f5c0cb50972b50c929b004a7496d3f2e2ded387bf870f89da25793bb44"
+     "d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
+     "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077"
+     default))
  '(package-selected-packages
-   '(bind-key counsel flycheck ivy lsp-mode lsp-treemacs markdown-mode
-	      nord-theme))
+   '(bind-key counsel flycheck ivy kanagawa-themes lsp-mode lsp-treemacs
+	      markdown-mode nord-theme))
  '(tool-bar-mode nil))
